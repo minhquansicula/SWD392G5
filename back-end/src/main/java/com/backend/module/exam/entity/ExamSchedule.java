be871c290367391e26e914377a1b0a7696e7b57a
luunguyen3112005@gmail.com <luunguyen3112005@gmail.com>
2026-09-15 00:39:28 +0700

    Adding DB script and configuring DB connection

---
+++ back-end/src/main/java/com/backend/module/exam/entity/ExamSchedule.java
@@ -0,0 +1,51 @@
+package com.backend.module.exam.entity;
+
+import com.backend.module.auth.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "exam_schedules")
+public class ExamSchedule {
+    @Id
+    @ColumnDefault("gen_random_uuid()")
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "exam_id")
+    private Exam exam;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "student_id")
+    private User student;
+
+    @Column(name = "scheduled_start_time")
+    private OffsetDateTime scheduledStartTime;
+
+    @Column(name = "scheduled_end_time")
+    private OffsetDateTime scheduledEndTime;
+
+    @Size(max = 50)
+    @ColumnDefault("'PENDING'")
+    @Column(name = "status", length = 50)
+    private String status;
+
+    @Column(name = "final_score", precision = 5, scale = 2)
+    private BigDecimal finalScore;
+
+
+}
---
+++ back-end/src/main/java/com/backend/module/exam/entity/Question.java
@@ -0,0 +1,38 @@
+package com.backend.module.exam.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "questions")
+public class Question {
+    @Id
+    @ColumnDefault("gen_random_uuid()")
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "course_id")
+    private Cours course;
+
+    @NotNull
+    @Column(name = "content", nullable = false, length = Integer.MAX_VALUE)
+    private String content;
+
+    @Size(max = 50)
+    @Column(name = "difficulty_level", length = 50)
+    private String difficultyLevel;
+
+
+}
